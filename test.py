c6d6305de1d2a70cd2e1a4192785cff371b72e46
Komrix <jgrans52@gmail.com>
2024-05-07 21:33:03 -0400

    casing

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import json, boto3, argparse
 
 def main(endpoint_name):
-    client = boto3.client('SageMaker-runtime', region_name='us-east-1')
+    client = boto3.client('sagemaker-runtime', region_name='us-east-1')
     payload = json.dumps({"inputs": "In 3 words, name the biggest mountain on earth?"})
     response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType="application/json", Body=payload)
     print("Response:", json.loads(response['Body'].read().decode()))
